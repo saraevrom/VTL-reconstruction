a3f3e12ba27e728541c85fcc31564e347455fbdc
Roman Saraev <saraevrom@gmail.com>
2023-03-20 20:27:57 +0300

    Fixed astronomy

--- tools/reconstructor/__init__.py
+++ tools/reconstructor/__init__.py
@@ -1,5 +1,10 @@
 from ..tool_base import ToolBase
-
+from vtl_common.localized_GUI import GridPlotter
 
 class ReconstructorTool(ToolBase):
     TOOL_KEY = "tools.reconstruction"
+
+    def __init__(self, master):
+        super().__init__(master)
+        self.track_plotter = GridPlotter(self)
+        self.track_plotter.pack(fill="both", expand=True)
