628f5151442a5e0eb2f18fab4fbc6127ed7721b7
Roman Saraev <saraevrom@gmail.com>
2023-09-08 17:15:45 +0300

    Added more mixed exp+lin lightcurves

--- tools/new_reconstructor/models/linear_track_model/light_curves.py
+++ tools/new_reconstructor/models/linear_track_model/light_curves.py
@@ -116,25 +116,60 @@ class ExponentialLC(LightCurve):
         return ys
 
 
-class ExpolinearLC(LightCurve):
+
+class LCPart(object):
+
+    @staticmethod
+    def get_lc(normed_x):
+        raise NotImplementedError
+
+    @staticmethod
+    def postprocess(normed_x):
+        raise NotImplementedError
+
+
+class LinearPart(LCPart):
+    @staticmethod
+    def get_lc(normed_x):
+        return (1 + normed_x)
+
+    @staticmethod
+    def postprocess(normed_x):
+        return (1 + normed_x)
+
+
+class ExpPart(LCPart):
+    @staticmethod
+    def get_lc(normed_x):
+        return pm.math.exp(normed_x)
+
+    @staticmethod
+    def postprocess(normed_x):
+        return np.exp(normed_x)
+
+
+class StitchedLC(LightCurve):
     E0 = E0_field()
     tau_left = tau_field()
     tau_right = tau_field()
     mu_k0 = DistributionField("normal", mu=0, sigma=1.0)
+    LEFT_PART = LCPart
+    RIGHT_PART = LCPart
+
 
     def get_lc(self, delta_k, k0, const_storage):
         tau1 = self.tau_left("τ_L")
         tau2 = self.tau_right("τ_R")
         mu_k0 = self.mu_k0("mu_LC_k0", const_storage)
 
-        mu = create_deterministic("mu_LC", k0+mu_k0, const_storage)
-        #mu = pm.Deterministic("mu_LC", k0 + mu_k0)  # Maximum position from start of frame
+        mu = create_deterministic("mu_LC", k0 + mu_k0, const_storage)
+        # mu = pm.Deterministic("mu_LC", k0 + mu_k0)  # Maximum position from start of frame
         e0 = self.E0("E0", const_storage)
-        delta_k_centered = (delta_k-mu_k0)
-        lpart = e0*pm.math.exp(delta_k_centered/tau1)
-        rpart = e0*(1 - delta_k_centered/tau2)
+        delta_k_centered = (delta_k - mu_k0)
+        lpart = e0 * self.LEFT_PART.get_lc(delta_k_centered / tau1)
+        rpart = e0 * self.RIGHT_PART.get_lc(-delta_k_centered / tau2)
         raw = pt.switch(delta_k_centered < 0, lpart, rpart)
-        mapped = pt.switch(raw>0, raw, 0)
+        mapped = pt.switch(raw > 0, raw, 0)
         return mapped
 
     def _postprocess(self, delta_k, k0, ax, trace, pmt, actual_x):
@@ -143,63 +178,29 @@ class ExpolinearLC(LightCurve):
         mu_k0 = trace.get_estimation("mu_LC_k0")
         delta_k_centered = delta_k - mu_k0
         e0 = trace.get_estimation("E0")
-        lpart = e0 * np.exp(delta_k_centered / tau1)
-        rpart = e0 * (1 - delta_k_centered / tau2)
+        lpart = e0 * self.LEFT_PART.postprocess(delta_k_centered / tau1)
+        rpart = e0 * self.RIGHT_PART.postprocess(-delta_k_centered / tau2)
         raw = np.where(delta_k_centered < 0, lpart, rpart)
-        res = np.where(raw>0, raw, 0)
+        res = np.where(raw > 0, raw, 0)
         ax.plot(actual_x, res, **PLOT_STYLES[pmt[0]])
         return res
 
 
+class ExpolinearLC(StitchedLC):
+    LEFT_PART = ExpPart
+    RIGHT_PART = LinearPart
 
+class LinexponentLC(StitchedLC):
+    LEFT_PART = LinearPart
+    RIGHT_PART = ExpPart
 
-    # class TriangularLC(LightCurve):
-#     E_max = E0_field()
-#     E_start = E0_field()
-#     E_end = E0_field()
-#     x_0 = DistributionField("normal", mu=0, sigma=1.0)
-#
-#     def get_lc(self, delta_k, k0):
-#         e_max = self.E_max("E_peak")
-#         e_start = self.E_start("E_start")
-#         e_end = self.E_start("E_end")
-#         mu = pm.Deterministic("mu_LC", k0+mu_k0)  # Maximum position from start of frame
-#         sigma = self.sigma("sigma_LC")
-#         return e0 * pm.math.exp(-(delta_k-mu_k0)**2/(2*sigma**2))
+class ExpExpLC(StitchedLC):
+    LEFT_PART = ExpPart
+    RIGHT_PART = ExpPart
 
-class ExpExpLC(LightCurve):
-    E0 = E0_field()
-    tau_left = tau_field()
-    tau_right = tau_field()
-    mu_k0 = DistributionField("normal", mu=0, sigma=1.0)
-
-    def get_lc(self, delta_k, k0, const_storage):
-        tau1 = self.tau_left("τ_L")
-        tau2 = self.tau_right("τ_R")
-        mu_k0 = self.mu_k0("mu_LC_k0", const_storage)
-
-        mu = create_deterministic("mu_LC", k0+mu_k0, const_storage)
-        #mu = pm.Deterministic("mu_LC", k0 + mu_k0)  # Maximum position from start of frame
-        e0 = self.E0("E0", const_storage)
-        delta_k_centered = (delta_k-mu_k0)
-        lpart = e0*pm.math.exp(delta_k_centered/tau1)
-        rpart = e0*pm.math.exp(-delta_k_centered/tau2)
-        raw = pt.switch(delta_k_centered < 0, lpart, rpart)
-        mapped = pt.switch(raw>0, raw, 0)
-        return mapped
-
-    def _postprocess(self, delta_k, k0, ax, trace, pmt, actual_x):
-        tau1 = trace.get_estimation("τ_L")
-        tau2 = trace.get_estimation("τ_R")
-        mu_k0 = trace.get_estimation("mu_LC_k0")
-        delta_k_centered = delta_k - mu_k0
-        e0 = trace.get_estimation("E0")
-        lpart = e0 * np.exp(delta_k_centered / tau1)
-        rpart = e0 * np.exp(-delta_k_centered / tau2)
-        raw = np.where(delta_k_centered < 0, lpart, rpart)
-        res = np.where(raw>0, raw, 0)
-        ax.plot(actual_x, res, **PLOT_STYLES[pmt[0]])
-        return res
+class LinLinLC(StitchedLC):
+    LEFT_PART = LinearPart
+    RIGHT_PART = LinearPart
 
 
 def create_lc_alter():
@@ -210,6 +211,8 @@ def create_lc_alter():
         SEL__gauss = GaussianLC().generate_subform()
         SEL__exp = ExponentialLC().generate_subform()
         SEL__expolinear = ExpolinearLC().generate_subform()
+        SEL__linexponent = LinexponentLC().generate_subform()
         SEL__expexp = ExpExpLC().generate_subform()
+        SEL__linlin = LinLinLC().generate_subform()
 
     return LC_Alter
